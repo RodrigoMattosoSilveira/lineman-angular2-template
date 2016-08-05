62fdf14b3633cd3cc985887cc46e8169d42e9852
Rodrigo Silveira <Rodrigo.Silveira@madronesoft.com>
2016-08-04 23:20:24 -0700

    v.0.0.4 cleaned up end to end tests to have one single sanity test for now

---
+++ spec-e2e/sanity_spec.js
@@ -0,0 +1,8 @@
+describe('sany test for now', function () {
+
+  describe('a universal test', function () {
+    it('add two numbers correctly', function() {
+      expect(1 + 1).toEqual(2);
+    });
+  });
+});
